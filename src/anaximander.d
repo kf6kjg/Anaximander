08129354c8e15cfe458186ef0935c1149178ec5e
Ricky Curtice <kf6kjg+bitbucket@gmail.com>
2014-08-03 19:39:27 -0700

    Added configuration file and a logging service.
    The logging service still needs to be able to write to disk, but that'll come when it's needed.
    
    JSON is easy to bring in, as the parsing facilities are part of the standard lib, but it's not very forgiving syntax wise nor does it have nice automatic connections between variables and keys.  Ah well, works for now.

--- src/anaximander.d
+++ src/anaximander.d
@@ -30,29 +30,34 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 // Standard imports.  Keep sorted.
+import std.file;
 import std.getopt;
+import std.json;
 import std.stdio;
 
 // Local imports.  Keep sorted.
+import alogger;
 import aversioninfo;
-
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 // Constants
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-
+const string LGRP_APP = "app";
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 // Globals
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+// Globals are evil,
+// Globals cause bugs,
+// Globals are quick and easy until the correct better way shows up.
 
-const string gConfigFilePath = "./anaximander.ini"; /// Where in the world is the config file?
+JSONValue[string] gConfigDocument;
 
 // Tile paths
-const string gNewTilePath = "./newtiles"; /// The path for the tiles that yet need processing.
-const string gMapTilePath = "./maptiles"; /// The final output location for all finished tiles.
-const string gTempTilePath = "./maptiles.temp"; /// Staging ground for tiles.
+string gNewTilePath = "./newtiles"; /// The path for the tiles that yet need processing.
+string gMapTilePath = "./maptiles"; /// The final output location for all finished tiles.
+string gTempTilePath = "./maptiles.temp"; /// Staging ground for tiles.
 
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
@@ -60,12 +65,64 @@ const string gTempTilePath = "./maptiles.temp"; /// Staging ground for tiles.
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 int main(string[] args) {
-	writeln("Anaximander the Grid Cartographer at your service!");
+	string config_file = "./anaximander.json"; /// Where in the world is the config file?
+	bool do_call_get_tiles = false;
 	
+	// Process commandline parameters.
 	getopt(args,
-		"version|V", function void() {writefln(" Version %d", VERSION);},
-		"gettiles", function void() {},
+		std.getopt.config.caseSensitive,
+		std.getopt.config.passThrough,
+		std.getopt.config.bundling,
+		"config", &config_file,
+		"gettiles", &do_call_get_tiles,
+		"logfile|l", &gLogFile,
+		"logging|L", &gLogLevel,
+		"quiet|q", function(){ gLogLevel = LOG_LEVEL.QUIET; },
+		"verbose|v", function(){ gLogLevel = LOG_LEVEL.VERBOSE; },
+		"version|V", function(){ stdout.writefln(" Version %d", VERSION); },
 	);
+	
+	// A friendly welcome.
+	info(LGRP_APP, "Anaximander the Grid Cartographer at your service!");
+	
+	// Go attempt to read the config file.
+	try {
+		auto config_data = config_file.readText();
+		try {
+			JSONValue json_root = parseJSON(config_data);
+			assert(json_root.type() == JSON_TYPE.OBJECT);
+			gConfigDocument = json_root.object;
+		}
+		catch (Exception) {
+			warn(LGRP_APP, "Invalid config file [", config_file, "]: Error parsing JSON format. Using defaults.");
+		}
+	}
+	catch (Exception) {
+		warn(LGRP_APP, "Config file not found [", config_file, "]. Using defaults.");
+	}
+	
+	// Process config data.
+	if (gConfigDocument.length) {
+		debug_log(LGRP_APP, "Parsing values from config file.");
+		if ("new_tile_path" in gConfigDocument) {
+			gNewTilePath = gConfigDocument["new_tile_path"].str;
+			chatter(LGRP_APP, "Using new tile folder from config file: ", gNewTilePath);
+		}
+		
+		if ("map_tile_path" in gConfigDocument) {
+			gMapTilePath = gConfigDocument["map_tile_path"].str;
+			chatter(LGRP_APP, "Using map tile folder from config file: ", gMapTilePath);
+		}
+		
+		if ("temp_tile_path" in gConfigDocument) {
+			gTempTilePath = gConfigDocument["temp_tile_path"].str;
+			chatter(LGRP_APP, "Using temp tile folder from config file: ", gTempTilePath);
+		}
+	}
+	
+	// If requested, gather tiles from regions.
+	
+	// Create the zoom levels.
 	
 	return 0;
 }
